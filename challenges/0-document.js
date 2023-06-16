680d7ad711ab7c3192482e27e14f7ef882eb5c95
Beth <bethan.hp@gmail.com>
2023-06-16 13:28:34 +0100

    0 done

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -1,4 +1,32 @@
-// TODO (Ex.0): document and write tests for this function
+/*
+Write a function that, given a number, returns a negative version of the number.
+
+(e.g. given 10, it should return -10; but given -10, it should still return -10 since it's already negative)
+
+function:
+=========
+name: asNegative
+
+parameters
+	- NUM: number
+
+return: number
+
+pseudo code:
+
+if NUM is greater than 0
+	return -NUM
+else
+	return NUM
+  */
+
+  /**
+   * Given a number returns the negative of it
+   * @param {number} n - the number inputted
+   * 
+   * @returns {number} the negative of n
+   */
+  // TODO (Ex.0): document and write tests for this function
 
 function asNegative(n) {
   // ...
